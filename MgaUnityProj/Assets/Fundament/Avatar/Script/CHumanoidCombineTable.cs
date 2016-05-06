6f8924e8ab9ed9fe26fe5b68501ee541d70c6ec6
Alexis Yang <nbalexis@gmail>
2016-05-06 16:33:07 +0800

    简直是丧心病狂的配色方案

---
+++ MgaUnityProj/Assets/Fundament/Avatar/Script/CHumanoidCombineTable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CHumanoidCombineTableElement : CMGDataElement
+{
+    public string[] m_sCombine = new string[0];
+
+    override public string GetString()
+    {
+        string sRet = base.GetString();
+        sRet += Write("Combine", m_sCombine);
+        return sRet;
+    }
+
+    override public void LoadData(string sTextToParse)
+    {
+        base.LoadData(sTextToParse);
+        m_sCombine = (string[])GetElementValue(sTextToParse, "Combine", m_sCombine);
+    }
+}
+
+public class CHumanoidCombineTable : TMGData<CHumanoidCombineTableElement>
+{
+    public override string GetDefaultSavePath()
+    {
+        return Application.dataPath + "/Fundament/Avatar/Resources/HumanoidCombineTable";
+    }
+
+    public override string GetDefaultLoadPath()
+    {
+        return "HumanoidCombineTable";
+    }
+}
--- MgaUnityProj/Assets/Fundament/Avatar/Script/CHumanoidDesc.cs
+++ MgaUnityProj/Assets/Fundament/Avatar/Script/CHumanoidDesc.cs
@@ -25,25 +25,16 @@ public enum EHumanoidWeapon : byte
     EHT_Max,
 }
 
-public enum EHumanoidDoc : byte
-{
-    EHD_Hair,
-    EHD_Hat,
-    EHD_Glass,
-}
-
 public enum EHumanoidComponentPos
 {
     ECP_Body,
     ECP_Back,
     ECP_Head,
     ECP_Hair,
-    ECP_HeadWear,
     ECP_Feet,
     ECP_Hand,
     ECP_Weapon,
     ECP_Wing,
-    ECP_BloodLineBack,
     ECP_BloodLineFront,
     ECP_Shadow,
 }
@@ -60,25 +51,23 @@ public enum EHumanoidSize
 public class CHumanoidDescElement : CMGDataElement
 {
     public string m_sObjectPath = "";
-    public string[] m_sDependency = new string[0];
+    public int m_iWeight = 1;
 
     public EHumanoidComponentPos m_ePos = EHumanoidComponentPos.ECP_Body;
     public EHumanoidType m_eHumanType = EHumanoidType.EHT_Both;
     public EHumanoidSide m_eHumanSide = EHumanoidSide.EHT_Left;
     public EHumanoidWeapon m_eHumanWeapon = EHumanoidWeapon.EHW_BareHand;
-    public EHumanoidDoc m_eHumanDocPos = EHumanoidDoc.EHD_Glass;
 
     override public string GetString()
     {
         string sRet = base.GetString();
 
         sRet += Write("ObjectPath", m_sObjectPath);
-        sRet += Write("Dependency", m_sDependency);
         sRet += Write("Pos", m_ePos);
+        sRet += Write("Weight", m_iWeight);
         sRet += Write("HumanType", m_eHumanType);
         sRet += Write("HumanSide", m_eHumanSide);
         sRet += Write("HumanWeapon", m_eHumanWeapon);
-        sRet += Write("HumanDocPos", m_eHumanDocPos);
 
         return sRet;
     }
@@ -87,12 +76,11 @@ public class CHumanoidDescElement : CMGDataElement
     {
         base.LoadData(sTextToParse);
         m_sObjectPath = (string)GetElementValue(sTextToParse, "ObjectPath", m_sObjectPath);
-        m_sDependency = (string[])GetElementValue(sTextToParse, "Dependency", m_sDependency);
+        m_iWeight = (int)GetElementValue(sTextToParse, "Pos", m_iWeight);
         m_ePos = (EHumanoidComponentPos)GetElementValue(sTextToParse, "Pos", m_ePos);
         m_eHumanType = (EHumanoidType)GetElementValue(sTextToParse, "HumanType", m_eHumanType);
         m_eHumanSide = (EHumanoidSide)GetElementValue(sTextToParse, "HumanSide", m_eHumanSide);
         m_eHumanWeapon = (EHumanoidWeapon)GetElementValue(sTextToParse, "HumanWeapon", m_eHumanWeapon);
-        m_eHumanDocPos = (EHumanoidDoc)GetElementValue(sTextToParse, "HumanDocPos", m_eHumanDocPos);
     }
 }
 
